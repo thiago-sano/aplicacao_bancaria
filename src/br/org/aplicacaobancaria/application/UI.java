2af17fe6979fea778064173ee809b90a373d6ee9
Thiago Sano <thiago.sano@outlook.com>
2025-02-17 09:26:38 -0300

    feat: improved UI class

--- src/br/org/aplicacaobancaria/application/UI.java
+++ src/br/org/aplicacaobancaria/application/UI.java
@@ -50,24 +50,87 @@ public class UI {
 
     public static void printBranchMenu(){
         clearScreen();
-        System.out.println("\nACESSO DE AGENCIA");
-        System.out.println("1 - CADASTRAR CONTA");
-        System.out.println("2 - ALTERAR LIMITE");
-        System.out.println("3 - VOLTAR");
-        System.out.print("Digite uma opcao: ");
+        int option = 0;
+        while (option != 3){
+            System.out.println("\nACESSO DE AGENCIA");
+            System.out.println("1 - CADASTRAR CONTA");
+            System.out.println("2 - ALTERAR LIMITE");
+            System.out.println("3 - VOLTAR");
+            System.out.print("Digite uma opcao: ");
+            option = readMenuOption();
+            switch (option){
+                case 1: {
+                    System.out.println("\nCADASTRO DE CONTA");
+                    return;
+                }
+                case 2: {
+                    System.out.println("\nALTERACAO DE LIMITE");
+                    return;
+                }
+                case 3: {
+                    System.out.println();
+                    printMainMenu();
+                    return;
+                }
+                default:{
+                    System.out.println("OPCAO INVALIDA\n");
+                    break;
+                }
+            }
+        }
+
     }
 
     public static void printClientMenu(){
         clearScreen();
-        System.out.println("\nACESSO DE CLIENTES");
-        System.out.println("1 - EXIBIR INFORMACOES DA CONTA");
-        System.out.println("2 - DEPOSITO");
-        System.out.println("3 - SAQUE");
-        System.out.println("4 - SOLICITAR ALTERACAO DE LIMITE");
-        System.out.println("5 - TRANSFERENCIA");
-        System.out.println("6 - EXTRATO");
-        System.out.println("7 - VOLTAR");
-        System.out.print("Digite uma opcao: ");
+        int option = 0;
+        while (option != 3){
+            System.out.println("\nACESSO DE CLIENTES");
+            System.out.println("1 - EXIBIR INFORMACOES DA CONTA");
+            System.out.println("2 - DEPOSITO");
+            System.out.println("3 - SAQUE");
+            System.out.println("4 - SOLICITAR ALTERACAO DE LIMITE");
+            System.out.println("5 - TRANSFERENCIA");
+            System.out.println("6 - EXTRATO");
+            System.out.println("7 - VOLTAR");
+            System.out.print("Digite uma opcao: ");
+            option = readMenuOption();
+            switch (option) {
+                case 1: {
+                    System.out.println("\nINFORMACOES DA CONTA");
+                    return;
+                }
+                case 2: {
+                    System.out.println("\nDEPOSITO");
+                    return;
+                }
+                case 3: {
+                    System.out.println("\nSAQUE");
+                    return;
+                }
+                case 4: {
+                    System.out.println("\nALTERACAO DE LIMITE");
+                    return;
+                }
+                case 5: {
+                    System.out.println("\nTRANSFERENCIA");
+                    return;
+                }
+                case 6: {
+                    System.out.println("\nEXTRATO");
+                    return;
+                }
+                case 7: {
+                    System.out.println();
+                    printMainMenu();
+                    return;
+                }
+                default: {
+                    System.out.println("OPCAO INVALIDA\n");
+                    break;
+                }
+            }
+        }
     }
 
     public static int readMenuOption(){
