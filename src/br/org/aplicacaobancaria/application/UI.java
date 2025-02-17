8de483df204f0b3d0ffddb9b803ae8e9b855c0d5
Thiago Sano <thiago.sano@outlook.com>
2025-02-17 08:40:29 -0300

    feat: UI class

---
+++ src/br/org/aplicacaobancaria/application/UI.java
@@ -0,0 +1,77 @@
+package br.org.aplicacaobancaria.application;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+public class UI {
+
+    Scanner sc = new Scanner(System.in);
+    public UI() {
+    }
+
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void printMainMenu() {
+        clearScreen();
+        int option = 0;
+        while (option != 3){
+            System.out.println("MENU PRINCIPAL");
+            System.out.println("1 - ACESSO AGENCIA");
+            System.out.println("2 - ACESSO CLIENTE");
+            System.out.println("3 - SAIR");
+            System.out.print("Digite uma opcao: ");
+            option = readMenuOption();
+            switch (option){
+                case 1: {
+                    printBranchMenu();
+                    return;
+                }
+                case 2: {
+                    printClientMenu();
+                    return;
+                }
+                case 3: {
+                    System.out.println("\nATE LOGO!!");
+                    exit(0);
+                }
+                default:{
+                    System.out.println("OPCAO INVALIDA\n");
+                    break;
+                }
+            }
+        }
+    }
+
+
+    public static void printBranchMenu(){
+        clearScreen();
+        System.out.println("\nACESSO DE AGENCIA");
+        System.out.println("1 - CADASTRAR CONTA");
+        System.out.println("2 - ALTERAR LIMITE");
+        System.out.println("3 - VOLTAR");
+        System.out.print("Digite uma opcao: ");
+    }
+
+    public static void printClientMenu(){
+        clearScreen();
+        System.out.println("\nACESSO DE CLIENTES");
+        System.out.println("1 - EXIBIR INFORMACOES DA CONTA");
+        System.out.println("2 - DEPOSITO");
+        System.out.println("3 - SAQUE");
+        System.out.println("4 - SOLICITAR ALTERACAO DE LIMITE");
+        System.out.println("5 - TRANSFERENCIA");
+        System.out.println("6 - EXTRATO");
+        System.out.println("7 - VOLTAR");
+        System.out.print("Digite uma opcao: ");
+    }
+
+    public static int readMenuOption(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+}
